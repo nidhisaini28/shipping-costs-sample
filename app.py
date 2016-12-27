a65d3e00a78d3b44653ae9c3967ea867da749c37
nidhisaini28 <saini.nidhi28@gmail.com>
2016-12-27 11:54:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,9 @@ import os
 from flask import Flask
 from flask import request
 from flask import make_response
-from googleplaces import GooglePlaces, types, lang
 
 # Flask app should start in global layout
 app = Flask(__name__)
-
-#YOUR_API_KEY = 'AIzaSyDFYyH5YoKVlY0BmbFUl5YLU3NGy6POKl8'
-#google_places = GooglePlaces(YOUR_API_KEY)
 
 
 @app.route('/webhook', methods=['POST'])
@@ -34,20 +30,11 @@ def makeWebhookResult(req):
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
- #   bank = parameters.get("bank")
-#    address = parameters.get("address")
-    
-#    query_result = google_places.nearby_search(location=address, keyword=bank,radius=2000, types=[types.TYPE_BANK])
-#    url_list=[]
-#    lat_long_list=[]
-#    for place in query_result.places:       
-#        url_list.append(place.url)
-#        lat_long_list.append(place.geo_location)
     zone = parameters.get("shipping-zone")
 
-#    cost = {'Europe':900, 'North America':1200, 'South America':1300, 'Asia':1400, 'Africa':1500}
+    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "please click the urls " 
+    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
     print("Response:")
     print(speech)
