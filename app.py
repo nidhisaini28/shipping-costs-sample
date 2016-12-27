92c84a166aace9ddb3e3aa19c777072064af1d63
nidhisaini28 <saini.nidhi28@gmail.com>
2016-12-27 16:32:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,9 +37,6 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("bank-name")
     address = parameters.get("address")
-    query_result = google_places.nearby_search(location='Gurugram, India', keyword='Raasta',radius=20000, types=[types.TYPE_FOOD])
-    for place in query_result.places:
-        the_url=place.url
     speech = "The cost of shipping to " + zone + " is " + address 
     
 
