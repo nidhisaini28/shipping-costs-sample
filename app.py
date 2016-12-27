1d27c84f874a0ca255214dde265c2dc967990eb6
nidhisaini28 <saini.nidhi28@gmail.com>
2016-12-27 12:11:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ def makeWebhookResult(req):
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
+    zone = parameters.get("bank-name")
+    #zone = parameters.get("shipping-zone")
 
     #cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
